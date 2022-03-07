a414098ffe39c81b5832bbcdba15ffd0f036688d
이성우 <lsw5244@naver.com>
2022-03-07 18:04:34 +0900

    feat : 3인칭 카메라의 조준모드 시 위치변경 기능 추가
    카메라의 조준기능 추가
    ALT를 눌러 좌 우를 둘러 볼 수 있는 기능 추가
    TODO : 조준모드 해제 시 로테이션도 부드럽게 변경되도록 바꾸기

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,38 +4,49 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    [SerializeField]
-    private Transform _cameraArmTransform;
+    public Transform _cameraArmTransform;
 
-    [SerializeField]
-    private float _cameraMoveSpeed = 200f;
+    public float _cameraMoveSpeed = 200f;
 
-    [SerializeField]
-    private Transform _camera;
+    public Transform _cameraTransform;
+
+    public Transform _aimModCameraTransfrom;
+
+    public Transform _normalModCameraTransfrom;
 
     void Start()
     {
-        
+        _cameraTransform = Camera.main.transform;
     }
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetKey(KeyCode.LeftAlt))
         {
-            _cameraArmTransform.Rotate(Input.GetAxis("Mouse Y") * _cameraMoveSpeed * Time.deltaTime
+            _cameraArmTransform.Rotate(/*Input.GetAxis("Mouse Y") * -_cameraMoveSpeed * Time.deltaTime*/0f
                 , Input.GetAxis("Mouse X") * _cameraMoveSpeed * Time.deltaTime
                 , 0f);
+
+            _cameraTransform.LookAt(transform.position);
         }
         else if(Input.GetMouseButton(1))
         {
+            //_cameraTransform.position = Vector3.zero;
 
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _aimModCameraTransfrom.position, 0.02f);
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, _aimModCameraTransfrom.rotation, 0.02f);
         }
         else
         {
-            _cameraArmTransform.rotation = Quaternion.Lerp(_cameraArmTransform.rotation,
-                 Quaternion.identity, 0.1f);    // 카메라 원래 위치로 돌리기
+            ResetCameraTransfrom();
         }
+    }
 
-        _camera.LookAt(transform.position);
+    void ResetCameraTransfrom()
+    {
+        _cameraArmTransform.rotation = Quaternion.Lerp(_cameraArmTransform.rotation,
+            Quaternion.identity, 0.1f);    // 카메라Arm 원래 위치로 돌리기
+        _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _normalModCameraTransfrom.position, 0.02f);
+        _cameraTransform.LookAt(transform.position);
     }
 }
