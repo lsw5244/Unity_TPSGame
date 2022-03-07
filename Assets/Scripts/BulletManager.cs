405dd72ab6ffffa7248bfae68f08a0c97d7f4a02
이성우 <lsw5244@naver.com>
2022-03-07 21:35:33 +0900

    feat : BulletManager.cs제작
    BulletManager.cs는 총알들을 생성하여 가지고 있고, 다른 객체의 요청에
    따라 사용하지 않는 총알을 리턴 해 줌

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bullet;
+
+    private GameObject[] _bulletPool = new GameObject[10];
+    private void Awake()
+    {
+        for(int i = 0; i < 10; ++i)
+        {
+            _bulletPool[i] = Instantiate(_bullet, Vector3.zero, Quaternion.identity);
+            _bulletPool[i].SetActive(false);
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        for(int i = 0; i < 10; ++i)
+        {
+            if(_bulletPool[i].activeSelf == true)
+            {
+                return _bulletPool[i];
+            }
+        }
+
+        return null;
+    }
+}
