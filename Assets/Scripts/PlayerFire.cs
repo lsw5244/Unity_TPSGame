10f15c6c1474fb04c1d68424941f95ac9dab1ac2
Ojingeo <33677312+lsw5244@users.noreply.github.com>
2022-03-08 21:22:15 +0900

    feat : 총알 발사 기능 추가 (#1)
    * feat : BulletManager.cs제작
    
    BulletManager.cs는 총알들을 생성하여 가지고 있고, 다른 객체의 요청에
    따라 사용하지 않는 총알을 리턴 해 줌
    
    * feat : 플레이어가 총알 오브젝트를 발사하는 기능 구현
    
    BulletManager에 있는 총알을 받아와서 발사시키는 기능을 하는
    PlayerFire스크립트 구현
    
    * Refactor : 총알을 발사하는 코드를 헬퍼 함수로 묶음
    
    * feat : 조준 중일때 총알을 발사하는 기능 추가
    
    조준중일 때 Ray를 쏳아 Ray가 hit한 곳을 향해 총알을 발사하도록 구현
    
    * Refactor : PlayerFire.cs 코드 리팩토링
    
    함수 이름 변경, 총알을 발사하는 부분을 하나의 함수로 통합

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _fireTransform;
+    [SerializeField]
+    private Transform _aimModCameraTransfrom;
+
+    [SerializeField]
+    private BulletManager _bulletManager;
+
+    [SerializeField]
+    private float _firePower;
+    [SerializeField]
+    private float _fireDelay;
+    [SerializeField]
+    private float _fireRayDistance;
+    
+    private bool _canFire;
+
+    void Start()
+    {
+        _canFire = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && _canFire == true)
+        {
+            StartCoroutine("StartDelay");
+
+            if(Input.GetMouseButton(1)) // 조준 모드일 때
+            {
+                RaycastHit hit;
+
+                Vector3 hitPosition = _aimModCameraTransfrom.position + _aimModCameraTransfrom.forward * _fireRayDistance;
+                Vector3 hitDirection = (hitPosition - _fireTransform.position).normalized;
+
+                if (Physics.Raycast(_aimModCameraTransfrom.position, _aimModCameraTransfrom.forward, out hit, _fireRayDistance))
+                {
+                    // 레이에 맞았을 때 ( 레이에 닿은 방향으로 날리기)
+                    hitDirection = (hit.point - _fireTransform.position).normalized;
+
+                    ShootBullet(hitDirection);
+                }
+                else
+                {
+                    // 레이에 안맞았을 때 ( 레이를 쏜 방향으로 날리기 )
+                    ShootBullet(hitDirection);
+                }
+            }
+            else
+            {
+                ShootBullet(_fireTransform.forward);
+            }
+
+        }
+    }
+
+    void ShootBullet(Vector3 shootDistance)
+    {
+        GameObject bullet = _bulletManager.GetBullet();
+
+        if (bullet != null)
+        {
+            bullet.transform.position = _fireTransform.position;
+            bullet.transform.rotation = transform.rotation;
+            bullet.SetActive(true);
+
+            bullet.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            bullet.GetComponent<Rigidbody>().AddForce(shootDistance * _firePower);
+        }
+    }
+
+    IEnumerator StartDelay()
+    {
+        _canFire = false;
+        yield return new WaitForSeconds(_fireDelay);
+        _canFire = true;
+    }
+}
