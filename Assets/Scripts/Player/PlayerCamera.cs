8a9bd5747012a747f62fdc331ac0529e77d8bf17
이성우 <lsw5244@naver.com>
2022-03-10 21:50:19 +0900

    feat : 조준모드 중 위 아래로 조준 할 수 있는 기능 추가
    TODO : 위나 아래로 180도 이상 돌아갈 때 카메라가 거꾸로 돌아가는 현상
    수정하기

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -7,21 +7,34 @@ public class PlayerCamera : MonoBehaviour
 {
     [SerializeField]
     private Transform _cameraArmTransform;
+
     [SerializeField]
     private float _cameraMoveSpeed = 200f;
+
     [SerializeField]
     private Transform _cameraTransform;
     [SerializeField]
     private Transform _aimModCameraTransfrom;
     [SerializeField]
     private Transform _normalModCameraTransfrom;
+
+    private Quaternion _aimModTransformRotation;
+
     [SerializeField]
     private Image _crosshairImage;  // TODO : UI매니저가 생기면 해당 변수와 기능을 UI매니저에서 하도록 구현하기
-    
+
+    private void Start()
+    {
+        _aimModTransformRotation = _aimModCameraTransfrom.rotation;
+    }
     void Update()
     {
         if (Input.GetMouseButton(1)) 
         {
+            _aimModCameraTransfrom.Rotate(Input.GetAxis("Mouse Y") * -_cameraMoveSpeed * 2f * Time.deltaTime
+                , 0f
+                , 0f);
+
             _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _aimModCameraTransfrom.position, 0.02f);
             _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, _aimModCameraTransfrom.rotation, 0.02f);
 
@@ -38,6 +51,15 @@ public class PlayerCamera : MonoBehaviour
         else if(Input.GetMouseButtonUp(1))
         {
             _crosshairImage.gameObject.SetActive(false);
+
+            Debug.Log($"변환 전 : {_aimModCameraTransfrom.rotation.eulerAngles}");
+
+            _aimModCameraTransfrom.rotation = Quaternion.Euler(180f, 
+                _aimModCameraTransfrom.rotation.eulerAngles.y,
+                _aimModCameraTransfrom.rotation.eulerAngles.z);
+
+            Debug.Log($"변환 후 : {_aimModCameraTransfrom.rotation.eulerAngles}");
+
         }
         else
         {
