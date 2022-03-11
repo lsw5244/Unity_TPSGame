a96a58904ee04a2034031e5efdb235327e3f96db
이성우 <lsw5244@naver.com>
2022-03-11 17:56:15 +0900

    feat : 조준모드 중 위 아래를 바라볼 수 있도록 구현
    조준모드 중 마우스 움직임을 통해 위, 아래를 조준할 수 있음
    최대로 올릴 수 있는 각도까지만 위 아래로 회전시킬 수 있도록 구현

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -10,6 +10,7 @@ public class PlayerCamera : MonoBehaviour
 
     [SerializeField]
     private float _cameraMoveSpeed = 200f;
+    public float verticalViewAngle = 30f;
 
     [SerializeField]
     private Transform _cameraTransform;
@@ -18,23 +19,26 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField]
     private Transform _normalModCameraTransfrom;
 
-    private Quaternion _aimModTransformRotation;
-
     [SerializeField]
     private Image _crosshairImage;  // TODO : UI매니저가 생기면 해당 변수와 기능을 UI매니저에서 하도록 구현하기
 
-    private void Start()
-    {
-        _aimModTransformRotation = _aimModCameraTransfrom.rotation;
-    }
+    private float _currentVerticalViewAngle = 0;
+    private Vector3 _aimModAngle;
+
     void Update()
     {
         if (Input.GetMouseButton(1)) 
         {
-            _aimModCameraTransfrom.Rotate(Input.GetAxis("Mouse Y") * -_cameraMoveSpeed * 2f * Time.deltaTime
-                , 0f
-                , 0f);
-
+            // 마우스 입력을 토대로 입력 값 저장
+            _currentVerticalViewAngle += Input.GetAxis("Mouse Y") * Time.deltaTime * -_cameraMoveSpeed * 2f;
+            _currentVerticalViewAngle = Mathf.Clamp(_currentVerticalViewAngle, -verticalViewAngle, verticalViewAngle);
+            // 입력값을 토대로 바라볼 각도 결정 
+            _aimModAngle = _aimModCameraTransfrom.eulerAngles;
+            _aimModAngle.x = _currentVerticalViewAngle;
+            _aimModCameraTransfrom.eulerAngles = _aimModAngle;
+            //_aimModCameraTransfrom.eulerAngles = new Vector3(temp, _aimModCameraTransfrom.eulerAngles.y, _aimModCameraTransfrom.eulerAngles.z);
+            
+            // 카메라 이동
             _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _aimModCameraTransfrom.position, 0.02f);
             _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, _aimModCameraTransfrom.rotation, 0.02f);
 
@@ -42,7 +46,9 @@ public class PlayerCamera : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.LeftAlt))
         {
-            _cameraArmTransform.Rotate(/*Input.GetAxis("Mouse Y") * -_cameraMoveSpeed * Time.deltaTime*/0f
+            _cameraTransform.position = _normalModCameraTransfrom.position;
+            // 카메라 피봇 회전
+            _cameraArmTransform.Rotate(0f
                 , Input.GetAxis("Mouse X") * _cameraMoveSpeed * Time.deltaTime
                 , 0f);
 
@@ -51,14 +57,12 @@ public class PlayerCamera : MonoBehaviour
         else if(Input.GetMouseButtonUp(1))
         {
             _crosshairImage.gameObject.SetActive(false);
-
-            Debug.Log($"변환 전 : {_aimModCameraTransfrom.rotation.eulerAngles}");
-
-            _aimModCameraTransfrom.rotation = Quaternion.Euler(180f, 
+            // 카메라 회전했던 값 리셋
+            _aimModCameraTransfrom.rotation = Quaternion.Euler(0f, 
                 _aimModCameraTransfrom.rotation.eulerAngles.y,
                 _aimModCameraTransfrom.rotation.eulerAngles.z);
-
-            Debug.Log($"변환 후 : {_aimModCameraTransfrom.rotation.eulerAngles}");
+            // 현제 바라보는 각도 초기화
+            _currentVerticalViewAngle = 0f;
 
         }
         else
