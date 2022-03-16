50e4b818ea3f80abd703c5b8f4616084993545e0
Ojingeo <33677312+lsw5244@users.noreply.github.com>
2022-03-16 17:39:45 +0900

    feat : 플레이어 프리팹 제작 (#7)
    * feat : 총 에셋 추가
    
    * feat : 플레이어에 총 오브젝트 추가 및 총을 잡고있도록 구현
    
    IK애니메이션을 이용하여 오른손은 그립, 왼손은 핸드가드를 잡도록 구현
    
    * feat : 플레이어에 카메라 기능 추가
    
    * feat : 플레이어의 이동 애니메이션이 나오도록 구현
    
    * feat : 카메라가 기본 모드일 때 캐릭터의 머리를 바라보도록 구현
    
    * fix : 캐릭터 이동 중 카메라가 흔들리는 현상 수정
    
    카메라가 캐리겉의 머리 본을 바라보는 것이 아니라 지정된 위치를
    바라보도록 수정
    
    * feat : 플레이어 프리팹 화

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _gunTransfom;
+    [SerializeField]
+    private Transform _gunGripTransfrom;
+    [SerializeField]
+    private Transform _gunHandGaurdTransfom;
+    private Animator _animator;
+
+    private float _horizontal = 0f;
+    private float _vertical = 0f;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            _animator.SetBool("AimMod", true);
+        }
+        if(Input.GetMouseButtonUp(1))
+        {
+            _animator.SetBool("AimMod", false);
+        }
+
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("xDir", _horizontal);
+        _animator.SetFloat("zDir", _vertical);
+
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+        _animator.SetIKPosition(AvatarIKGoal.RightHand, _gunGripTransfrom.position);
+
+        _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+        _animator.SetIKPosition(AvatarIKGoal.LeftHand, _gunHandGaurdTransfom.position);
+
+    }
+
+}
