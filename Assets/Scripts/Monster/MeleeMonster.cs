54b67ed164121cea5bdca12a7aba6a51484ac395
이성우 <lsw5244@naver.com>
2022-03-19 16:39:12 +0900

    feat : 기본 근접 몬스터가 사용할 MeleeMonster.cs제작

---
+++ Assets/Scripts/Monster/MeleeMonster.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeMonster : MonoBehaviour
+{
+    enum State
+    {
+        Idle, Trace, Attack, Hit, Die
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
