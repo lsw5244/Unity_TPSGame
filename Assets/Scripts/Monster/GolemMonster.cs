07bed3a9dd0af0f0b61cfae34c10a602c68dd028
이성우 <lsw5244@naver.com>
2022-03-27 18:29:57 +0900

    feat : GolemMonster의 기본 애니메이션 컨트롤러 설정

--- Assets/Scripts/Monster/GolemMonster.cs
+++ Assets/Scripts/Monster/GolemMonster.cs
@@ -67,31 +67,31 @@ public class GolemMonster : Monster, IMonster
 
     public override void Attack()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     public override void Die()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     public void GetDamage(float damage)
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     public override void Idle()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     public void PoisonEffect(float damage)
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 
     public override void Trace()
     {
-        throw new System.NotImplementedException();
+        //throw new System.NotImplementedException();
     }
 }
