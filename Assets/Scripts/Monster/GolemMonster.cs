591fe17312a71e0b4e2792b326b1c4a6b87661ef
이성우 <lsw5244@naver.com>
2022-03-27 18:19:00 +0900

    feat : GolemMonster.cs에 변수, 기본 TraceCheck구현

--- Assets/Scripts/Monster/GolemMonster.cs
+++ Assets/Scripts/Monster/GolemMonster.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GolemMonster : Monster, IMonster
 {
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+    private BoxCollider _attackCollider;
+
+    public float attackPower = 10f;
 
     void Start()
     {
+        currentHp = _maxHP;
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _attackCollider = GetComponent<BoxCollider>();
 
+        _attackCollider.enabled = false;
+
+        _playerTransform = GameObject.Find("Player").transform;/* Find("Player");*/
+        StartCoroutine(TraceCheck());
     }
 
-    void Update()
+    IEnumerator TraceCheck()
     {
+        while (_isAlive == true)
+        {
+            float distance = Vector3.Distance(transform.position, _playerTransform.position);
 
+            if (distance < attackDistance)
+            {
+                _currentState = State.Attack;
+            }
+            else if (distance < traceDistance)
+            {
+                _currentState = State.Trace;
+            }
+            else
+            {
+                _currentState = State.Idle;
+            }
+
+            switch (_currentState)
+            {
+                case State.Attack:
+                    Attack();
+                    break;
+                case State.Trace:
+                    Trace();
+                    break;
+                case State.Idle:
+                    Idle();
+                    break;
+            }
+
+            //if (_animator.GetCurrentAnimatorStateInfo(0).IsName("hit") == true)
+            //{
+            //    _navMeshAgent.velocity = Vector3.zero;
+            //}
+
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 
     public override void Attack()
