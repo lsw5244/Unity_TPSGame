15789f480564d47e1b5a3a99c7cb481682fb9db9
이성우 <lsw5244@naver.com>
2022-03-27 17:09:13 +0900

    feat : GolemMonster.cs제작
    GolemMonsterㄱ가 사용 할 스크립트 생성

---
+++ Assets/Scripts/Monster/GolemMonster.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemMonster : Monster, IMonster
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void GetDamage(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Idle()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void PoisonEffect(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Trace()
+    {
+        throw new System.NotImplementedException();
+    }
+}
